ad843eab06bd929245629a0b7946f1c407223911
Irshad <amitorproperties@gmail.com>
2022-11-05 10:46:49 +0530

    first commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<amitor_sms/AmitorSmsPlugin.h>)
+#import <amitor_sms/AmitorSmsPlugin.h>
+#else
+@import amitor_sms;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AmitorSmsPlugin registerWithRegistrar:[registry registrarForPlugin:@"AmitorSmsPlugin"]];
+}
+
+@end
